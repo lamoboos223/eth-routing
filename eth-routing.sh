defbcb7a8747eb8841f88dcb080a7f274335dbce
Lama <lamo.boos@hotmail.com>
2022-08-29 12:01:25 +0300

    Add files via upload

---
+++ eth-routing.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+cat << "EOF"
+ _                              ____   _____ 
+| |                            / __ \ / ____|
+| |     __ _ _ __ ___   __ _  | |  | | (___  
+| |    / _\ | '_ ` _ \ / _` | | |  | |\___ \ 
+| |___| (_| | | | | | | (_| | | |__| |____) |
+|______\__,_|_| |_| |_|\__,_|  \____/|_____/
+
+       _,.
+     ,` -.)
+    '( _/'-\\-.               
+   /,|`--._,-^|            ,     
+   \_| |`-._/||          ,'|       
+     |  `-, / |         /  /      
+     |     || |        /  /       
+      `r-._||/   __   /  /  
+  __,-<_     )`-/  `./  /
+ '  \   `---'   \   /  / 
+     |           |./  /  
+     /           //  /     
+ \_/' \         |/  /         
+  |    |   _,^-'/  /              
+  |    , ``  (\/  /_        
+   \,.->._    \X-=/^         
+   (  /   `-._//^`  
+    `Y-.____(__}              
+     |     {__)           
+           ()`
+
+EOF
+
+echo "* What is the Default Gateway ip address for eth3? (Use ipconfig to get it)"
+read x
+echo "* What is the Subnet Mask for eth3? (Use ipconfig to get it)"
+read y
+z=0
+while [ $z != 'q' ] 
+do 
+echo "* What is the ip address you with to route to? (Use ipconfig to get it)"
+read z
+route add $z MASK $y $x -p
+
+done
+
+netstat -rn
+echo "Thank you for using eth-routing tool. Have a good day"
